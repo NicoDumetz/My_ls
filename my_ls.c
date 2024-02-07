25b4d1cfaafecda9785141f5da93f3de03f8b7a1
NicoDumetz <nicolas.dumetz@epitech.eu>
2024-02-07 23:24:59 +0100

    [~] add sort aplha when displa

--- my_ls.c
+++ my_ls.c
@@ -7,6 +7,15 @@
 #include "include/my_ls.h"
 #include "include/my_printf.h"
 
+int error(DIR* fd)
+{
+    if (fd == NULL) {
+        printf("%s\n", strerror(errno));
+        exit(0);
+        return 84;
+    }
+}
+
 static int add_information(struct flags *flags,
     struct dir *buffer, DIR* fd, char *path)
 {
@@ -18,7 +27,7 @@ static int add_information(struct flags *flags,
         if ((entity->d_name[0] == '.' && flags->a == 0 && flags->d == 0))
             entity = readdir(fd);
         else {
-            buffer[i].d = entity->d_type == 4 ? 'd' : '-';
+            buffer[i].d = entity->d_type == 4 ? 'd' : entity->d_type == '-';
             buffer[i].name = entity->d_name;
             buffer[i].reclen = entity->d_reclen;
             add_perm(entity, buffer, i, path);
@@ -41,51 +50,42 @@ static void get_flags(char *str, struct flags *flags)
     }
 }
 
-static char *get_av(char **av, char *path, struct flags *flags)
-{
-    int bo = 0;
-
-    for (int i = 1; av[i]; i++) {
-        if (av[i][0] != '-') {
-            path = av[i];
-            bo = 1;
-        } else
-            get_flags(av[i], flags);
-    }
-    if ( bo == 0)
-        path = ".";
-    return path;
-}
-
-int error(DIR* fd)
-{
-    if (fd == NULL) {
-        printf("%s\n", strerror(errno));
-        exit(0);
-        return 84;
-    }
-}
-
-int my_ls(int ac, char **av)
+int my_ls(char *path, struct flags *flags)
 {
     DIR* fd;
     struct dir *buffer;
-    struct flags flags;
-    char *path = get_av(av, path, &flags);
 
     fd = opendir(path);
     if (error(fd) == 84)
         return 84;
     buffer = malloc(sizeof(struct dir) * 500000);
-    if (add_information(&flags, buffer, fd, path) == 84)
+    if (add_information(flags, buffer, fd, path) == 84)
         return 84;
-    display(buffer, &flags, path);
+    display(buffer, flags, path);
     closedir(fd);
     free(buffer);
     return 1;
 }
 
+static char *get_av(char **av)
+{
+    int bo = 0;
+    struct flags flags;
+
+    for (int i = 1; av[i]; i++) {
+        if (av[i][0] != '-') {
+            my_ls(av[i], &flags);
+            bo = 1;
+        } else
+            get_flags(av[i], &flags);
+    }
+    if ( bo == 0) {
+        my_ls(".", &flags);
+    }
+}
+
 int main(int ac, char **av)
 {
-    return my_ls(ac, av);
+    get_av(av);
+    return 1;
 }
